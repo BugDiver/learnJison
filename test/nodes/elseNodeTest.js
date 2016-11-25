c156cb5b1178f90ada1c4fe62688237ab701821c
BugDiver <vinaysh@thoughtworks.com>
2016-11-25 10:50:45 +0530

    rename file.

--- test/nodes/elseNodeTest.js
+++ test/nodes/elseNodeTest.js
@@ -4,7 +4,7 @@ var AssignNode = require('../../src/nodes/assignmentNode.js');
 var OperatorNode = require('../../src/nodes/operatorNode.js');
 var NumberNode = require('../../src/nodes/numberNode.js');
 var IdNode = require('../../src/nodes/idNode.js');
-var ElseNode = require('../../src/nodes/esleNode.js');
+var ElseNode = require('../../src/nodes/elseNode.js');
 
 describe('ElseNode',function(){
 	describe('toString',function(){
