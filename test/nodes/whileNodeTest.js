6d370a4e76be6f9934c4c3eeae7c58f713eefe5b
BugDiver <vinaysh@thoughtworks.com>
2016-11-28 14:18:15 +0530

    fixing test name.

--- test/nodes/whileNodeTest.js
+++ test/nodes/whileNodeTest.js
@@ -9,13 +9,13 @@ var BooleanNode = require('../../src/nodes/booleanNode.js');
 
 describe('WhileNode',function(){
 	describe('toString',function(){
-		it('should have js equivalent of a condition statement',function(){
+		it('should have js equivalent of a loop statement',function(){
 		 	var loop = new WhileNode(new BooleanNode('true'),[new AssignNode(new IdNode('x'),new NumberNode(2))]);
 
 		 	expect(loop.toString()).to.be.eql('while(true){var x = 2;}')
 		});
 
-		it('should have js equivalent of a condition statement with a expression',function(){
+		it('should have js equivalent of a loop statement with a expression',function(){
 			var x = new IdNode('x');
 			var two = new NumberNode(2);
 
